d36204fc44ac427c89bc1528af5158af88d6429c
ahang <176976712@qq.com>
2021-03-06 21:35:20 +0800

    尝试

--- cloud-consul-provider-payment8006/src/main/java/com/ah/springcloud/controller/PaymentConsulController.java
+++ cloud-consul-provider-payment8006/src/main/java/com/ah/springcloud/controller/PaymentConsulController.java
@@ -13,6 +13,7 @@ public class PaymentConsulController {
 
     @Value("${server.port}")
     private String serverPort;
+    private String name;
 
     @GetMapping("/payment/consul")
     public String paymentInfo(){

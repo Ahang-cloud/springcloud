0c25070120e1a0c76e29023bd0bedbc9862eea2e
ahang <176976712@qq.com>
2021-03-06 21:58:34 +0800

    zz

--- cloud-consul-provider-payment8006/src/main/java/com/ah/springcloud/controller/PaymentConsulController.java
+++ cloud-consul-provider-payment8006/src/main/java/com/ah/springcloud/controller/PaymentConsulController.java
@@ -13,7 +13,7 @@ public class PaymentConsulController {
 
     @Value("${server.port}")
     private String serverPort;
-    private String abcs;
+    private String bbb;
 
     @GetMapping("/payment/consul")
     public String paymentInfo(){
